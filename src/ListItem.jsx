4a883bb2925811cb73fef2081e9a92713b2e0f49
1jack80 <work.davidmainoo@gmail.com>
2023-05-07 15:19:38 +0000

    use custom hook (useUserData) for user image and names in issue instances

--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -1,6 +1,12 @@
 import { GoComment, GoIssueOpened } from "react-icons/go";
+import { relativeDate } from "./helpers/relativeDate";
+import { useUserData } from "./helpers/useUserData";
 
 export function ListItem({ createdBy, createdDate, title, labels, number, assignee }) {
+	const createdByUser = useUserData(createdBy);
+
+	const assigneeUser = useUserData(assignee);
+
 	return (
 		<li className="issueItem">
 			<span className="">
@@ -8,20 +14,30 @@ export function ListItem({ createdBy, createdDate, title, labels, number, assign
 			</span>
 			<div>
 				<header className="issueHeader">
-					<h4 className="issueTitle">JQuery looks wierd on weekends</h4>
-					<span className="issueLabel ">question</span>
+					<h4 className="issueTitle">{title}</h4>
+					{labels.map((label) => (
+						<span
+							className="issueLabel "
+							key={label}>
+							{label}
+						</span>
+					))}
 				</header>
-				<p className="issueDetails">#998 opened 3 hours ago by Tanner</p>
+				<small className="issueDetails">
+					#${number} opened {relativeDate(createdDate)} by{" "}
+					{createdByUser.isSuccess ? createdByUser.data.name : ""}
+				</small>
 			</div>
 			<img
 				className="issueAssignee"
-				src="https://res.cloudinary.com/uidotdev/image/twitter_name/tylermcginnis"
-				alt="Avatar of assignee"
+				src={assigneeUser.isSuccess ? assigneeUser.data.profilePictureUrl : ""}
+				// alt="assignee avatar "
 			/>
 
+			{/* <span>{assignee}</span> */}
 			<div className="issueCommentCount">
 				<GoComment className="commentIcon" />
-				<span className="commentCount">281</span>
+				<span className="commentCount"></span>
 			</div>
 		</li>
 	);
