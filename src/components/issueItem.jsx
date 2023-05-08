3a82da0f6674390ab1c10fb9385f1ca1e7757a79
1jack80 <work.davidmainoo@gmail.com>
2023-05-08 00:37:59 +0000

    rename listItem to issueItem

--- src/components/issueItem.jsx
+++ src/components/issueItem.jsx
@@ -1,44 +1,42 @@
 import { GoComment, GoIssueOpened } from "react-icons/go";
-import { relativeDate } from "./helpers/relativeDate";
-import { useUserData } from "./helpers/useUserData";
+import { relativeDate } from "../helpers/relativeDate.js";
+import { useUserData } from "../helpers/useUserData.js";
 
 export function IssueItem({ createdBy, createdDate, title, labels, number, assignee }) {
-	const createdByUser = useUserData(createdBy);
+  const createdByUser = useUserData(createdBy);
 
-	const assigneeUser = useUserData(assignee);
+  const assigneeUser = useUserData(assignee);
 
-	return (
-		<li className="issueItem">
-			<span className="">
-				<GoIssueOpened className="issueIcon" />
-			</span>
-			<div>
-				<header className="issueHeader">
-					<h4 className="issueTitle">{title}</h4>
-					{labels.map((label) => (
-						<span
-							className="issueLabel "
-							key={label}>
-							{label}
-						</span>
-					))}
-				</header>
-				<small className="issueDetails">
-					#${number} opened {relativeDate(createdDate)} by{" "}
-					{createdByUser.isSuccess ? createdByUser.data.name : ""}
-				</small>
-			</div>
-			<img
-				className="issueAssignee"
-				src={assigneeUser.isSuccess ? assigneeUser.data.profilePictureUrl : ""}
-				// alt="assignee avatar "
-			/>
+  return (
+    <li className="issueItem">
+      <span className="">
+        <GoIssueOpened className="issueIcon" />
+      </span>
+      <div>
+        <header className="issueHeader">
+          <h4 className="issueTitle">{title}</h4>
+          {labels.map((label) => (
+            <span
+              className="issueLabel "
+              key={label}>
+              {label}
+            </span>
+          ))}
+        </header>
+        <small className="issueDetails">
+          #${number} opened {relativeDate(createdDate)} by{" "}
+          {createdByUser.isSuccess ? createdByUser.data.name : ""}
+        </small>
+      </div>
+      <img
+        className="issueAssignee"
+        src={assigneeUser.isSuccess ? assigneeUser.data.profilePictureUrl : ""}
+      />
 
-			{/* <span>{assignee}</span> */}
-			<div className="issueCommentCount">
-				<GoComment className="commentIcon" />
-				<span className="commentCount"></span>
-			</div>
-		</li>
-	);
+      <div className="issueCommentCount">
+        <GoComment className="commentIcon" />
+        <span className="commentCount"></span>
+      </div>
+    </li>
+  );
 }
